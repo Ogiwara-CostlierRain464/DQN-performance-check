c1e41ddd65fa6a8b25ae02c7b0c006dfc586d700
ogiwara <yushiogiwara@gmail.com>
2019-12-30 23:19:22 +0800

    ass typing

--- agent.py
+++ agent.py
@@ -1,3 +1,5 @@
+import numpy as np
+from typing import Union
 from table_brain import TableBrain
 from transition import Transition
 from nn_brain import NNBrain
@@ -7,9 +9,9 @@ class Agent:
     def __init__(self, num_states, num_actions):
         self.brain = NNBrain(num_states, num_actions)
 
-    def update_q_function(self, trn):
+    def update_q_function(self, trn) -> None:
         self.brain.update_q_function(trn)
 
-    def get_action(self, state, episode):
+    def get_action(self, state: np.ndarray, episode) -> Union[int, float, bool]:
         """行動の決定"""
         return self.brain.decide_action(state, episode)
