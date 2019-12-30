c688a27c86382682367ce2696e4ba2a4c8767412
ogiwara <yushiogiwara@gmail.com>
2019-12-30 21:27:29 +0800

    update NN Brain

--- nn_brain.py
+++ nn_brain.py
@@ -1,7 +1,10 @@
 import random
+import torch.nn.functional as F
+import torch
 from torch import nn
 from torch import optim
 from typing import List
+import numpy as np
 
 from table_brain import Brain
 from transition import Transition
@@ -10,6 +13,8 @@ from transition import Transition
 class NNBrain(Brain):
     capacity = 10000
     batch_size = 32
+    # 時間割引率
+    gamma = 0.99
 
     def __init__(self, num_states, num_actions):
         self.num_actions = num_actions
@@ -32,14 +37,58 @@ class NNBrain(Brain):
         if len(self.memory) < self.batch_size:
             return
 
+        # List[Transition]
         trns = self.memory.sample(self.batch_size)
+        # Transition(List[int], List[int], List[int], List[int])
+        batch = Transition(*zip(*trns))
+
+        # (FloatTensor[1x4])xBATCH_SIZE
+        # => FloatTensor[BATCH_SIZEx4]
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+        non_final_next_states = torch.cat([
+            s for s in batch.next_state if s is not None
+        ])
+
+        self.model.eval()
+        state_action_values = self.model(state_batch).gather(1, action_batch)
+        non_final_mask = torch.ByteTensor(
+            tuple(map(lambda s: s is not None, batch.next_state))
+        )
+
+        next_state_values = torch.zeros(self.batch_size)
+
+        next_state_values[non_final_mask] = self.model(
+            non_final_next_states
+        ).max(1)[0].detach()
+
+        expected_state_action_values = reward_batch + self.gamma * next_state_values
+
+        self.model.train()
+
+        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
 
     def update_q_function(self, transition: Transition):
         pass
 
-    def decide_action(self, observation, step):
-        pass
+    def decide_action(self, observation, episode):
+        epsilon = 0.5 * (1 / (episode + 1))
 
+        if epsilon <= np.random.uniform(0, 1):
+            self.model.eval()
+
+            with torch.no_grad():
+                action = self.model(observation).max(1)[1].view(1, 1)
+        else:
+            action = torch.LongTensor([[ random.randrange(self.num_actions) ]])
+
+        return action
 
 class ReplayMemory:
     def __init__(self, capacity):
